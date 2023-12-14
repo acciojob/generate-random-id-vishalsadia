6ad8c44d741cf5ccbe68cfd289283d2b40b0530c
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2023-12-14 16:38:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function makeid(l) {
-  // write your code here
-}
+  if (typeof l !== 'number' || l <= 0 || l > 1000 || !Number.isInteger(l)) {
+    throw new Error('Invalid input: Please provide a positive integer less than or equal to 1000.');
+  }
 
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+
+  for (let i = 0; i < l; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
 // Do not change the code below.
 const l = prompt("Enter a number.");
-alert(makeid(l));
+alert(makeid(l)); 
